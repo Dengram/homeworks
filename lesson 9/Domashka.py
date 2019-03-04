ec739a4e950a72e37cfb50d15e6d9230601f8ee3
Dengram <47913757+Dengram@users.noreply.github.com>
2019-03-04 20:17:48 +0300

    Домашка

--- lesson 9/Domashka.py
+++ lesson 9/Domashka.py
@@ -1,17 +1,25 @@
-text = input().split()
-result = []
-imax = 0
+import random
 
-for j in range(0, len(text)):
-    for i in range(0, len(text) - 1):
-        if len(text[imax]) < len(text[i]):
-            imax = i
-            result.append(text[imax])
+class Person:
+    firstName = 'Петя'
+    lastName = 'Петров'
+    age = '17'
 
-        print(text)
-           
+    def __init__(self, firstName, lastName, age):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
 
-        
+    def __str__(self):
+        return 'Имя ' + str(self.firstName) + 'Фамилия ' + str(self.lastName) + 'Возраст ' + str(self.age)
 
-        
+person1 = ('Петя', 'Петров', '17')
+
+fraza1 = 'Програмирование - моё всё'
+fraza2 = 'Блин, через 10 минут придёт Вова, а я определения не записал'
+fraza3 = 'Не зделаю дз, Вова скажет папе, мне хана'
+fraza = random.randomint(fraza1, fraza2, fraza3)
+
+print(person1)
+print('Любимые фразы: ' + fraza)
 
